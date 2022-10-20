c2312b645fd7adc0c9ddb2c48379d54029dd4807
Ehtisham <ehtisham@gmail.com>
2022-10-20 22:21:33 +0500

    Deployed

---
+++ precache-manifest.dc80dd994c951aa9d95a039b31602942.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a58efecd4d3c27e72c1c",
+    "url": "/static/css/main.c72604cf.chunk.css"
+  },
+  {
+    "revision": "a58efecd4d3c27e72c1c",
+    "url": "/static/js/main.a58efecd.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "0f01fca20ab23c16f6be",
+    "url": "/static/js/2.0f01fca2.chunk.js"
+  },
+  {
+    "revision": "7ee0e2da555c3fd35366",
+    "url": "/static/js/runtime~main.7ee0e2da.js"
+  },
+  {
+    "revision": "18f70361ae86d4c94543ab7d00311818",
+    "url": "/index.html"
+  }
+];
